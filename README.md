66e0d6abea4740b241b2b6103ea72aff16a7d8ca
Mostafa <mostafa.omrane@proton.me>
2023-04-11 06:40:43 +0200

    Add a custom UI for Share Extensions without storyboard

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # tempo
+
+Tutorials that helped me :
+https://diamantidis.github.io/2020/01/11/share-extension-custom-ui
--- sharetempo/ShareViewController.swift
+++ sharetempo/ShareViewController.swift
@@ -10,5 +10,50 @@ import UIKit
 class ShareViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // 1: Set the background and call the function to create the navigation bar
+        self.view.backgroundColor = .systemGray6
+        setupNavBar()
+        setupViews()
+    }
+    
+    // 2: Set the title and the navigation items
+    private func setupNavBar() {
+        self.navigationItem.title = "My app"
+        
+        let itemCancel = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(cancelAction))
+        self.navigationItem.setLeftBarButton(itemCancel, animated: false)
+        
+        let itemDone = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(doneAction))
+        self.navigationItem.setRightBarButton(itemDone, animated: false)
+    }
+    
+    // 3: Define the actions for the navigation items
+    @objc private func cancelAction () {
+        let error = NSError(domain: "some.bundle.identifier", code: 0, userInfo: [NSLocalizedDescriptionKey: "An error description"])
+        extensionContext?.cancelRequest(withError: error)
+    }
+    
+    @objc private func doneAction() {
+        extensionContext?.completeRequest(returningItems: [], completionHandler: nil)
+    }
+    
+    private lazy var textField: UITextField = {
+        let textField = UITextField()
+        textField.text = "some value"
+        textField.backgroundColor = .white
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        
+        return textField
+    }()
+    
+    private func setupViews() {
+        self.view.addSubview(textField)
+        NSLayoutConstraint.activate([
+            textField.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            textField.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+            textField.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+            textField.heightAnchor.constraint(equalToConstant: 44)
+        ])
     }
 }
