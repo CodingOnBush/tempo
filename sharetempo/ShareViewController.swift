6114ba36a4b6108ef652794389ca6db3637898a5
Mostafa <mostafa.omrane@proton.me>
2023-04-16 07:26:17 +0200

    Trying to clean the code about share extension

--- sharetempo/ShareViewController.swift
+++ sharetempo/ShareViewController.swift
@@ -11,37 +11,13 @@ import MobileCoreServices
 
 class ShareViewController: UIViewController {
     
-    // loadView: "If you create your views manually, you must override this method and use it to create your views."
     override func loadView() {
         super.loadView()
-        
         self.setupView1()
     }
     
-    // viewDidLoad: "This method is called after the view controller has loaded its associated views into memory. This method is called regardless of whether the views were stored in a nib file or created programmatically in the loadView method."
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        //        let myCoreData = CoreData.shared
-        //
-        //        myCoreData.getStoredDataFromCoreData()
-        //        print(URL.storeURL(for: "TEST1", databaseName: "tempo"))
-        //        myCoreData.saveContext()
-        
-        //        print(getContextUrl())
-        //        let stringURL = getContextUrl().description
-        //        self.textField.placeholder = stringURL
-        //        let newItem = Item(context: viewContext)
-        //        newItem.timestamp = getContextUrl().description
-        //
-        //        do {
-        //            try viewContext.save()
-        //        } catch {
-        //            // Replace this implementation with code to handle the error appropriately.
-        //            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-        //            let nsError = error as NSError
-        //            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-        //        }
     }
     
     private func setupNavBar() {
@@ -55,7 +31,7 @@ class ShareViewController: UIViewController {
     }
     
     @objc private func cancelAction () {
-        let error = NSError(domain: "some.bundle.identifier", code: 0, userInfo: [NSLocalizedDescriptionKey: "An error description"])
+        let error = NSError(domain: "com.vegapunk.tempo.extension", code: 0, userInfo: [NSLocalizedDescriptionKey: "An error description"])
         extensionContext?.cancelRequest(withError: error)
     }
     
@@ -63,24 +39,15 @@ class ShareViewController: UIViewController {
         extensionContext?.completeRequest(returningItems: [], completionHandler: nil)
     }
     
-    private lazy var textField: UITextField = {
-        let textField = UITextField()
-        textField.backgroundColor = .clear
-        textField.translatesAutoresizingMaskIntoConstraints = false
-        textField.placeholder = getContextUrl().description
-        return textField
-    }()
-    
     private func setupView1() {
         self.view.backgroundColor = .init(red: 0.0, green: 0.2, blue: 0.0, alpha: 1)
+        
+        self.setupNavBar()
         
         let textField = UITextField()
         textField.backgroundColor = .clear
         textField.translatesAutoresizingMaskIntoConstraints = false
         textField.placeholder = "placeholder"
-        
-        self.setupNavBar()
-        
         self.view.addSubview(textField)
         
         let button = UIButton(frame: CGRect(x: 100, y: 100, width: 100, height: 50))
@@ -98,11 +65,12 @@ class ShareViewController: UIViewController {
     }
     
     @objc func buttonAction(sender: UIButton!) {
-        //        self.view.backgroundColor = .red
-        let speedLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 21))
+        let speedLabel = UILabel(frame: CGRect(x: 0, y: 200, width: 300, height: 521))
         speedLabel.text = "url ?"
         speedLabel.textColor = .white
-        speedLabel.textAlignment = .center
+        speedLabel.lineBreakMode = .byWordWrapping
+        speedLabel.numberOfLines = 0
+        speedLabel.textAlignment = .left
         self.view.addSubview(speedLabel)
         
         if let item = extensionContext?.inputItems.first as? NSExtensionItem,
@@ -112,45 +80,8 @@ class ShareViewController: UIViewController {
                 if let shareURL = url as? URL {
                     // do what you want to do with shareURL
                     speedLabel.text = shareURL.description
-                    
                 }
-//                self.extensionContext?.completeRequest(returningItems: [], completionHandler:nil)
             }
         }
-        
-        
     }
-    
-    private func setupView2() {
-        let viewFrame = CGRect(x: 0, y: 0, width: 150, height: 300)
-        self.view = UIView(frame: viewFrame)
-        self.view.backgroundColor = UIColor.clear
-        
-        let width: CGFloat = UIScreen.main.bounds.size.width
-        let height: CGFloat = UIScreen.main.bounds.size.height
-        let newView = UIView(frame: CGRect(x: (width * 0.10), y: (height * 0.10), width: (width * 0.75), height: (height / 2)))
-        newView.backgroundColor = UIColor.yellow
-        self.view.addSubview(newView)
-    }
-    
-    func getContextUrl() -> URL {
-        var contextUrl = URL(string: "")
-        
-        if let item = extensionContext?.inputItems.first as? NSExtensionItem,
-           let itemProvider = item.attachments?.first as? NSItemProvider,
-           itemProvider.hasItemConformingToTypeIdentifier("public.url") {
-            itemProvider.loadItem(forTypeIdentifier: "public.url", options: nil) { (url, error) in
-                if let shareURL = url as? URL {
-                    // do what you want to do with shareURL
-                    //                    stringUrl = try String(contentsOf: shareURL)
-                    contextUrl = shareURL
-                }
-                self.extensionContext?.completeRequest(returningItems: [], completionHandler:nil)
-            }
-        }
-        
-        return contextUrl!
-    }
-    
-    
 }
