d9a9f1d780cc43462df79da0d61d90129d73bdb0
Mostafa <mostafa.omrane@proton.me>
2023-04-16 06:03:52 +0200

    Getting current URL but too slowly

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Links that helped me :
 https://diamantidis.github.io/2020/01/11/share-extension-custom-ui
 https://hellocode.co/blog/post/share-extension/
 https://stackoverflow.com/questions/26091463/today-extension-failed-to-inherit-coremedia-permissions-from
+https://stackoverflow.com/a/75682775
--- sharetempo/ShareViewController.swift
+++ sharetempo/ShareViewController.swift
@@ -6,18 +6,42 @@
 //
 
 import UIKit
+import CoreData
+import MobileCoreServices
 
 class ShareViewController: UIViewController {
+    
     // loadView: "If you create your views manually, you must override this method and use it to create your views."
     override func loadView() {
         super.loadView()
+        
         self.setupView1()
     }
     
     // viewDidLoad: "This method is called after the view controller has loaded its associated views into memory. This method is called regardless of whether the views were stored in a nib file or created programmatically in the loadView method."
     override func viewDidLoad() {
         super.viewDidLoad()
-//        print(getContextUrl())
+        
+        //        let myCoreData = CoreData.shared
+        //
+        //        myCoreData.getStoredDataFromCoreData()
+        //        print(URL.storeURL(for: "TEST1", databaseName: "tempo"))
+        //        myCoreData.saveContext()
+        
+        //        print(getContextUrl())
+        //        let stringURL = getContextUrl().description
+        //        self.textField.placeholder = stringURL
+        //        let newItem = Item(context: viewContext)
+        //        newItem.timestamp = getContextUrl().description
+        //
+        //        do {
+        //            try viewContext.save()
+        //        } catch {
+        //            // Replace this implementation with code to handle the error appropriately.
+        //            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+        //            let nsError = error as NSError
+        //            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+        //        }
     }
     
     private func setupNavBar() {
@@ -43,20 +67,58 @@ class ShareViewController: UIViewController {
         let textField = UITextField()
         textField.backgroundColor = .clear
         textField.translatesAutoresizingMaskIntoConstraints = false
-        textField.placeholder = "Test"
+        textField.placeholder = getContextUrl().description
         return textField
     }()
     
     private func setupView1() {
-        self.view.backgroundColor = .white
+        self.view.backgroundColor = .init(red: 0.0, green: 0.2, blue: 0.0, alpha: 1)
+        
+        let textField = UITextField()
+        textField.backgroundColor = .clear
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.placeholder = "placeholder"
+        
         self.setupNavBar()
+        
         self.view.addSubview(textField)
+        
+        let button = UIButton(frame: CGRect(x: 100, y: 100, width: 100, height: 50))
+        button.backgroundColor = .green
+        button.setTitle("Test Button", for: .normal)
+        button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+        self.view.addSubview(button)
+        
         NSLayoutConstraint.activate([
             textField.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
             textField.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
             textField.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
             textField.heightAnchor.constraint(equalToConstant: 44)
         ])
+    }
+    
+    @objc func buttonAction(sender: UIButton!) {
+        //        self.view.backgroundColor = .red
+        let speedLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 21))
+        speedLabel.text = "url ?"
+        speedLabel.textColor = .white
+        speedLabel.textAlignment = .center
+        self.view.addSubview(speedLabel)
+        
+        if let item = extensionContext?.inputItems.first as? NSExtensionItem,
+           let itemProvider = item.attachments?.first as? NSItemProvider,
+           itemProvider.hasItemConformingToTypeIdentifier("public.url") {
+            itemProvider.loadItem(forTypeIdentifier: "public.url", options: nil) { (url, error) in
+                if let shareURL = url as? URL {
+                    // do what you want to do with shareURL
+                    speedLabel.text = shareURL.description
+                    
+                }
+//                self.extensionContext?.completeRequest(returningItems: [], completionHandler:nil)
+            }
+        }
+        
+        
     }
     
     private func setupView2() {
@@ -80,7 +142,7 @@ class ShareViewController: UIViewController {
             itemProvider.loadItem(forTypeIdentifier: "public.url", options: nil) { (url, error) in
                 if let shareURL = url as? URL {
                     // do what you want to do with shareURL
-//                    stringUrl = try String(contentsOf: shareURL)
+                    //                    stringUrl = try String(contentsOf: shareURL)
                     contextUrl = shareURL
                 }
                 self.extensionContext?.completeRequest(returningItems: [], completionHandler:nil)
@@ -89,4 +151,6 @@ class ShareViewController: UIViewController {
         
         return contextUrl!
     }
+    
+    
 }
